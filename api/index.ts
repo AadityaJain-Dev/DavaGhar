be0cadf8568955302d137ba858ee7b0ad7ad18e8
Aditya Jain <61523512+AadityaJain-Dev@users.noreply.github.com>
2025-03-09 16:56:53 +0530

    binding KV and DB

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,14 @@
 export default {
-  fetch(request, env) {
-    const url = new URL(request.url);
+  async fetch(request, env) {
+    const url = new URL(request.url);    
 
     if (url.pathname.startsWith("/api/")) {
+      await env.KV.put("KEY2", "VALUE2");
+      const value = await env.KV.get("KEY2");
+      console.log(value);
       return Response.json({
         name: "Cloudflare",
+        v: value
       });
     }
 
